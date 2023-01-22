1b344c868e184f7175db73aa9754e0d966129726
Christopher Esterlein <christopheresterlein@Christophers-MacBook-Pro.local>
2023-01-21 18:48:34 -0800

    fast moving re-arranging

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Home from './containers/Home';
+
 
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />}>
-          <Route path="*" element={<Home />} />
-        </Route>
-      </Routes>
+      <h2>This the the app container</h2>
     </div>
   );
 }
