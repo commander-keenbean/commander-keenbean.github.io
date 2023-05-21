f0ad031cbd8386c23bf7bb639eb48638df0471b5
Christopher Esterlein <christopheresterlein@Christophers-MacBook-Pro.local>
2023-05-20 23:46:49 -0600

    prettier

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,58 +2,67 @@ import { useEffect, useState } from 'react';
 import { Container, Button } from 'react-bootstrap';
 
 const fastForwardStyleContainer = {
-    display: 'flex',
-    justifyContent: 'flex-end',
-    marginRight: '20px',
+  display: 'flex',
+  justifyContent: 'flex-end',
+  marginRight: '20px',
 };
 
 const fastForwardStyle = {
-    backgroundColor: 'black',
-    borderColor: '#0F0',
+  backgroundColor: 'black',
+  borderColor: '#0F0',
 };
 
 const Home = () => {
-    const [text, setText] = useState('');
-    const [isTyping, setIsTyping] = useState(false);
-    const [fastForward, setFastForward] = useState(false);
-    // this text must start with a space
-    const fullText = " This application was developed entirely with the help of ChatGPT, OpenAI's language model. Through interactive conversations, the model assisted in providing helpful suggestions and insights, influencing key decision-making processes, and offering reliable solutions to encountered issues.";
-  
-    const completeText = () => {
-        setFastForward(true);
-    };
+  const [text, setText] = useState('');
+  const [isTyping, setIsTyping] = useState(false);
+  const [fastForward, setFastForward] = useState(false);
+  // this text must start with a space
+  const fullText =
+    " This application was developed entirely with the help of ChatGPT, OpenAI's language model. Through interactive conversations, the model assisted in providing helpful suggestions and insights, influencing key decision-making processes, and offering reliable solutions to encountered issues.";
 
-    useEffect(() => {
-        let index = 0;
-        setIsTyping(true);
-        const timer = setInterval(() => {
-          if (fastForward) {
-            setText(fullText);
-            setIsTyping(false);
-            clearInterval(timer);
-          } else {
-            setText((prevText) => prevText + fullText.charAt(index));
-            index++;
-            if (index >= fullText.length) {
-              setIsTyping(false);
-              clearInterval(timer);
-            }
-          }
-        }, 50);
-        return () => clearInterval(timer);
-      }, [fastForward]);
+  const completeText = () => {
+    setFastForward(true);
+  };
+
+  useEffect(() => {
+    let index = 0;
+    setIsTyping(true);
+    const timer = setInterval(() => {
+      if (fastForward) {
+        setText(fullText);
+        setIsTyping(false);
+        clearInterval(timer);
+      } else {
+        setText((prevText) => prevText + fullText.charAt(index));
+        index++;
+        if (index >= fullText.length) {
+          setIsTyping(false);
+          clearInterval(timer);
+        }
+      }
+    }, 50);
+    return () => clearInterval(timer);
+  }, [fastForward]);
 
   return (
     <Container className="p-3">
-        <h1 className="header" style={{color: 'white'}}>Welcome to my github pages</h1>
-        <h5>
-          {text}
-          {isTyping && <span className="cursor">|</span>}
-        </h5>
-        <br />
-        {isTyping && <div style={fastForwardStyleContainer}><Button style={fastForwardStyle} onClick={completeText}>{`>>`}</Button></div>}
-        <br />
-
+      <h1 className="header" style={{ color: 'white' }}>
+        Welcome to my github pages
+      </h1>
+      <h5>
+        {text}
+        {isTyping && <span className="cursor">|</span>}
+      </h5>
+      <br />
+      {isTyping && (
+        <div style={fastForwardStyleContainer}>
+          <Button
+            style={fastForwardStyle}
+            onClick={completeText}
+          >{`>>`}</Button>
+        </div>
+      )}
+      <br />
     </Container>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,37 +2,39 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Properties } from 'csstype';
 
-const navBarStyle : Properties<string | number, string & {}> = {
-    marginTop: '10px',
-    marginBottom: '40px',
-    paddingLeft: '20px',
+const navBarStyle: Properties<string | number, string & {}> = {
+  marginTop: '10px',
+  marginBottom: '40px',
+  paddingLeft: '20px',
 };
 
-const navBarBrandStyle : Properties<string | number, string & {}> = {
-    color: 'white', 
-    borderRight: 'solid',
-    borderColor: 'red',
-    paddingRight: '20px',
+const navBarBrandStyle: Properties<string | number, string & {}> = {
+  color: 'white',
+  borderRight: 'solid',
+  borderColor: 'red',
+  paddingRight: '20px',
 };
 
-const navBarLinkStyle : string = JSON.stringify({
-    color: 'white', 
+const navBarLinkStyle: string = JSON.stringify({
+  color: 'white',
 });
 
 const NavigationBar = () => {
   return (
     <Navbar style={navBarStyle} bg="dark" expand="lg">
-      <Navbar.Brand style={ navBarBrandStyle } href="#home">Commander Keenbean</Navbar.Brand>
+      <Navbar.Brand style={navBarBrandStyle} href="#home">
+        Commander Keenbean
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <LinkContainer to="/" style={navBarLinkStyle} >
-            <Nav.Link >Home</Nav.Link>
-          </LinkContainer> 
-          <LinkContainer to="/about" style={navBarLinkStyle} >
+          <LinkContainer to="/" style={navBarLinkStyle}>
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/about" style={navBarLinkStyle}>
             <Nav.Link>About</Nav.Link>
           </LinkContainer>
-          <LinkContainer to="/contact" style={navBarLinkStyle} >
+          <LinkContainer to="/contact" style={navBarLinkStyle}>
             <Nav.Link>Contact</Nav.Link>
           </LinkContainer>
         </Nav>
