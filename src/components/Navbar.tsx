31a21e35b16f6ca2d23582766fa89079cc6c37e4
Christopher Esterlein <christopheresterlein@Christophers-MacBook-Pro.local>
2023-05-20 23:34:22 -0600

    add gh pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,23 @@
 import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav } from 'react-bootstrap';
+import { Properties } from 'csstype';
 
-const navBarStyle = {
+const navBarStyle : Properties<string | number, string & {}> = {
     marginTop: '10px',
     marginBottom: '40px',
     paddingLeft: '20px',
 };
 
-const navBarBrandStyle = {
+const navBarBrandStyle : Properties<string | number, string & {}> = {
     color: 'white', 
     borderRight: 'solid',
     borderColor: 'red',
     paddingRight: '20px',
 };
+
+const navBarLinkStyle : string = JSON.stringify({
+    color: 'white', 
+});
 
 const NavigationBar = () => {
   return (
@@ -21,13 +26,13 @@ const NavigationBar = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <LinkContainer to="/" style={{color: 'white'}} >
+          <LinkContainer to="/" style={navBarLinkStyle} >
             <Nav.Link >Home</Nav.Link>
           </LinkContainer> 
-          <LinkContainer to="/about" style={{color: 'white'}} >
+          <LinkContainer to="/about" style={navBarLinkStyle} >
             <Nav.Link>About</Nav.Link>
           </LinkContainer>
-          <LinkContainer to="/contact" style={{color: 'white'}} >
+          <LinkContainer to="/contact" style={navBarLinkStyle} >
             <Nav.Link>Contact</Nav.Link>
           </LinkContainer>
         </Nav>
